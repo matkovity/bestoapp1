425e47fac3f0e4b8beb37fe97233cd9944471fef
norbert <mnorbyx1@gmail.com>
2026-07-04 16:21:51 +0200

    first commit

---
+++ app/src/main/java/com/example/bestoapp1/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.bestoapp1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import com.example.bestoapp1.ui.screens.FavoritesScreen
+import com.example.bestoapp1.ui.screens.HomeScreen
+import com.example.bestoapp1.ui.screens.ProfileScreen
+import com.example.bestoapp1.ui.theme.Bestoapp1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Bestoapp1Theme {
+                Bestoapp1App()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun Bestoapp1App() {
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            painterResource(it.icon),
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        when (currentDestination) {
+            AppDestinations.HOME -> HomeScreen()
+            AppDestinations.FAVORITES -> FavoritesScreen()
+            AppDestinations.PROFILE -> ProfileScreen()
+        }
+    }
+}
+
+enum class AppDestinations(
+    val label: String,
+    val icon: Int,
+) {
+    HOME("Home", R.drawable.ic_home),
+    FAVORITES("Favorites", R.drawable.ic_favorite),
+    PROFILE("Profile", R.drawable.ic_account_box),
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Bestoapp1Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/bestoapp1/ui/screens/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.bestoapp1.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Home Screen")
+}
